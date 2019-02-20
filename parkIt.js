7b3672fc4b302033ef37377f92a406f1e3648d8e
eobuya <eobuya@pointpark.edu>
2019-02-20 14:57:34 -0500

    Update

---
+++ parkIt.js
@@ -0,0 +1,12 @@
+
+var latitude;
+var longitude;
+var parkedLatitude;
+var parkLongitude;
+
+var storage;
+
+function init(){
+  document.addEventListener("deviceReady",onDeviceReady, false);
+  storage = window.localStorage;
+}
